feat: correct rotation

model/model_gp_bbox.py
@@ -1,6 +1,6 @@
 import bpy
 import numpy as np
-from mathutils import Vector
+from mathutils import Vector, Euler
 from typing import Sequence, Union, ClassVar, Optional, Literal
 from dataclasses import dataclass, field
 
@@ -21,7 +21,9 @@ class GreasePencilLayerBBox(GreasePencilProperty):
     min_x: float = 0
     max_y: float = 0
     min_y: float = 0
+    center: Vector = Vector((0, 0, 0))
     #
+    layer: bpy.types.GPencilLayer = None
     last_layer_index: int = None
 
     @property
@@ -29,15 +31,20 @@ class GreasePencilLayerBBox(GreasePencilProperty):
         """Return the 3d size of the bounding box."""
         return self.max_x - self.min_x, self.max_y - self.min_y
 
+    def rotation_2d(self) -> float:
+        """Return the rotation of the layer.
+        notice that the rotation is stored in the layer.rotation, but the value is the inverse of the actual rotation
+        see EditGreasePencilLayer in model_gp_edit.py for storing the rotation
+        """
+        return self.layer.rotation[2] if self.layer else 0
+
+    def rotation_2d_inverse(self) -> float:
+        return -self.layer.rotation[2] if self.layer else 0
+
     @property
     def size_v2d(self) -> Vector:
         """Return the 2d view size of the bounding box."""
         return VecTool.loc3d_2_v2d(self.size)
-
-    @property
-    def center(self) -> Vector:
-        """Return the 3d center of the bounding box."""
-        return Vector(((self.min_x + self.max_x) / 2, (self.min_y + self.max_y) / 2))
 
     @property
     def center_v2d(self) -> Vector:
@@ -70,10 +77,18 @@ class GreasePencilLayerBBox(GreasePencilProperty):
         return self.max_x, self.min_y
 
     @property
-    def bbox_points_3d(self) -> tuple[Vector, Vector, Vector, Vector]:
+    def bbox_points_3d(self, apply_rotate: bool = True) -> tuple[Vector, Vector, Vector, Vector]:
         """Return the bounding box points."""
         # top_left, top_right, bottom_left, bottom_right
-        return Vector(self.top_left), Vector(self.top_right), Vector(self.bottom_left), Vector(self.bottom_right)
+        points = Vector(self.top_left), Vector(self.top_right), Vector(self.bottom_left), Vector(self.bottom_right)
+        if apply_rotate:
+            angle = self.rotation_2d()
+            pivot_3d = self.center.to_3d()
+            points_3d = [p.to_3d() for p in points]
+            # rotate
+            points_3d = [(p - pivot_3d) @ Euler((0, 0, angle), 'XYZ').to_matrix() + pivot_3d for p in points_3d]
+            points = [Vector(p).to_2d() for p in points_3d]
+        return points
 
     @property
     def bbox_points_v2d(self) -> tuple[Vector, Vector, Vector, Vector]:
@@ -117,22 +132,65 @@ class GreasePencilLayerBBox(GreasePencilProperty):
         return new_points
 
     @staticmethod
-    def _calc_stroke_bbox(stroke: bpy.types.GPencilStroke) -> tuple[float, float, float, float]:
+    def _calc_stroke_bbox(stroke: bpy.types.GPencilStroke, rotate_z: float) -> tuple[
+        float, float, float, float, Vector]:
         """
         Calculate the bounding box of a stroke.
         :param stroke:
+        :param rotate_z: rotate back the stroke
         :return:
         """
-        with EditGreasePencilStroke.stroke_points(stroke) as vertices:
-            max_xyz_id = np.argmax(vertices, axis=0)
-            min_xyz_id = np.argmin(vertices, axis=0)
+        with EditGreasePencilStroke.stroke_points(stroke) as points:
+            pivot = np.mean(points, axis=0)
+            if rotate_z:  # if 0, no need to rotate
+                angle = rotate_z
+                points = ((points - pivot) @ np.array([[np.cos(angle), -np.sin(angle), 0],
+                                                       [np.sin(angle), np.cos(angle), 0],
+                                                       [0, 0, 1]]) + pivot)
+            max_xyz_id = np.argmax(points, axis=0)
+            min_xyz_id = np.argmin(points, axis=0)
 
-            max_x = float(vertices[max_xyz_id[0], 0])
-            max_y = float(vertices[max_xyz_id[1], 1])
-            min_x = float(vertices[min_xyz_id[0], 0])
-            min_y = float(vertices[min_xyz_id[1], 1])
+            max_x = float(points[max_xyz_id[0], 0])
+            max_y = float(points[max_xyz_id[1], 1])
+            min_x = float(points[min_xyz_id[0], 0])
+            min_y = float(points[min_xyz_id[1], 1])
 
-        return max_x, min_x, max_y, min_y
+        return max_x, min_x, max_y, min_y, Vector(pivot)
+
+    def _calc_layer_bbox(self, frame: bpy.types.GPencilFrame, rotate_z=float) -> None:
+        """
+        Calculate the bounding box of the grease pencil annotation layer.
+        :param frame: calc this frame
+        """
+        points = self._get_layer_points(frame)
+        pivot = np.mean(points, axis=0)
+        if rotate_z:  # if 0, no need to rotate
+            angle = rotate_z
+            points = ((points - pivot) @ np.array([[np.cos(angle), -np.sin(angle), 0],
+                                                   [np.sin(angle), np.cos(angle), 0],
+                                                   [0, 0, 1]]) + pivot)
+
+        max_xyz_id = np.argmax(points, axis=0)
+        min_xyz_id = np.argmin(points, axis=0)
+
+        max_x = float(points[max_xyz_id[0], 0])
+        max_y = float(points[max_xyz_id[1], 1])
+        min_x = float(points[min_xyz_id[0], 0])
+        min_y = float(points[min_xyz_id[1], 1])
+
+        return max_x, min_x, max_y, min_y, Vector(pivot)
+
+    def _get_layer_points(self, frame) -> np.ndarray:
+        """
+        Return the points of all the strokes in one numpy array.
+        """
+
+        all_points = []
+        for stroke in frame.strokes:
+            with EditGreasePencilStroke.stroke_points(stroke) as points:
+                all_points.append(points)
+
+        return np.concatenate(all_points, axis=0)
 
     def calc_active_layer_bbox(self) -> None:
         """
@@ -155,6 +213,7 @@ class GreasePencilLayerBBox(GreasePencilProperty):
         layer = self._get_layer(layer_name_or_index)
         if not layer:
             raise ValueError(f'Layer {layer_name_or_index} not found.')
+        self.layer = layer
 
         try:
             frame = layer.frames[0]
@@ -162,19 +221,21 @@ class GreasePencilLayerBBox(GreasePencilProperty):
             self.max_x = self.min_x = self.max_y = self.min_y = 0
             return
 
-        x_list = []
-        y_list = []
-        for stroke in frame.strokes:
-            max_x, min_x, max_y, min_y = self._calc_stroke_bbox(stroke)
-            x_list.extend([max_x, min_x])
-            y_list.extend([max_y, min_y])
+        points = self._get_layer_points(frame)
+        pivot = np.mean(points, axis=0)
 
-        if not x_list or not y_list:
-            self.max_x = self.min_x = self.max_y = self.min_y = 0
-            return
+        max_xyz_id = np.argmax(points, axis=0)
+        min_xyz_id = np.argmin(points, axis=0)
 
-        self.max_x = max(x_list)
-        self.min_x = min(x_list)
-        self.max_y = max(y_list)
-        self.min_y = min(y_list)
+        max_x = float(points[max_xyz_id[0], 0])
+        max_y = float(points[max_xyz_id[1], 1])
+        min_x = float(points[min_xyz_id[0], 0])
+        min_y = float(points[min_xyz_id[1], 1])
+
+        self.max_x = max_x
+        self.min_x = min_x
+        self.max_y = max_y
+        self.min_y = min_y
+        self.center = Vector(pivot)
+        # rotate the bounding box back
         self.last_layer_index = [i for i, l in enumerate(self.gp_data.layers) if l == layer][0]