e5b4b49e823ade50d331abcbd35cec516bb5136d
1029920178@qq.com <1029910278@qq.com>
2024-07-04 22:41:25 +0800

    feat: add tool

--- workspace_tool/tool_gp.py
+++ workspace_tool/tool_gp.py
@@ -1,7 +1,27 @@
 import bpy
 from ..public_path import get_tool_icon
 from ..operator.ops_gp_modal import ENN_OT_gp_set_active_layer, ENN_OT_gp_drag_modal
-from ..operator.ops_gp_basic import ENN_OT_add_gp, ENN_OT_remove_gp, ENN_OT_scale_gp,ENN_OT_gp_set_active_layer_color
+from ..operator.ops_gp_basic import ENN_OT_add_gp, ENN_OT_remove_gp, ENN_OT_scale_gp, ENN_OT_gp_set_active_layer_color
+
+
+class ENN_TL_gp_add(bpy.types.WorkSpaceTool):
+    bl_idname = "enn.gp_add_tool"
+    bL_idname_fallback = "node.select_box"
+    bl_space_type = 'NODE_EDITOR'
+    bl_context_mode = None
+    bl_label = "Add"
+    bl_icon = get_tool_icon('gp_add_tool')
+    bl_keymap = (
+        (ENN_OT_add_gp.bl_idname,
+         {"type": 'LEFTMOUSE', "value": 'PRESS', "shift": False, "ctrl": False},
+         {"properties": [('use_mouse_pos', True)]}
+         ),
+        (ENN_OT_add_gp.bl_idname,
+         {"type": 'LEFTMOUSE', "value": 'PRESS', "shift": True, "ctrl": False},
+         {"properties": [('use_mouse_pos', True), ('add_type', 'OBJECT')]}
+         ),
+
+    )
 
 
 # noinspection PyPep8Naming
@@ -10,16 +30,12 @@ class ENN_TL_gp_edit(bpy.types.WorkSpaceTool):
     bL_idname_fallback = "node.select_box"
     bl_space_type = 'NODE_EDITOR'
     bl_context_mode = None
-    bl_label = "Draw"
+    bl_label = "Move"
     bl_icon = get_tool_icon('gp_edit_tool')
     bl_keymap = (
         (ENN_OT_gp_set_active_layer.bl_idname,
          {"type": "LEFTMOUSE", "value": "CLICK"},
          {"properties": []},  # [("deselect_all", True)]
-         ),
-        (ENN_OT_add_gp.bl_idname,
-         {"type": 'A', "value": 'PRESS', "shift": True, "ctrl": False},
-         {"properties": [('use_mouse_pos', True), ('add_type', 'TEXT')]}
          ),
         # normal mode drag
         (ENN_OT_gp_drag_modal.bl_idname,
@@ -50,8 +66,6 @@ class ENN_TL_gp_edit(bpy.types.WorkSpaceTool):
         (ENN_OT_scale_gp.bl_idname,
          {"type": 'F', "value": 'PRESS', "ctrl": False, "alt": False, "shift": True},
          {"properties": [('scale_vector', (1, -1))]}),
-
-
     )
 
 
@@ -74,12 +88,14 @@ class ENN_TL_gp_color(bpy.types.WorkSpaceTool):
 def reigster():
     from bpy.utils import register_class, register_tool
 
-    register_tool(ENN_TL_gp_edit, separator=True)
+    register_tool(ENN_TL_gp_add, separator=True)
+    register_tool(ENN_TL_gp_edit, separator=False)
     register_tool(ENN_TL_gp_color, separator=False)
 
 
 def unregister():
     from bpy.utils import unregister_class, unregister_tool
 
+    unregister_tool(ENN_TL_gp_add)
     unregister_tool(ENN_TL_gp_edit)
     unregister_tool(ENN_TL_gp_color)
