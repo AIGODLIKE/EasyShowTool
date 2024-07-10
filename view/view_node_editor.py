68a452731ccc32c4b13c81055aab6b7efaee6110
1029920178@qq.com <1029910278@qq.com>
2024-07-10 15:25:30 +0800

    cleanup

--- view/view_node_editor.py
+++ view/view_node_editor.py
@@ -1,20 +1,10 @@
-import gpu
-import gpu.state
 import gpu.shader
 import bpy
-import blf
-import numpy as np
-import bmesh
-from gpu_extras.batch import batch_for_shader
-from gpu_extras.presets import draw_circle_2d
 from mathutils import Color, Vector
 
 from typing import Sequence, Union, ClassVar, Literal, Optional, Callable
 from dataclasses import dataclass, field
-from pathlib import Path
-from collections import OrderedDict
 
-from ..public_path import get_pref
 from ..model.model_gp_bbox import GPencilLayerBBox
 from ..model.model_draw import DrawData, DrawPreference
 from ..view_model.view_model_drag import DragGreasePencilViewModal
@@ -30,14 +20,14 @@ class ViewDrawHandle:
     log: ClassVar[list[str]] = []
 
     def add_to_node_editor(self, func: Union['ViewBasic', Callable], args: tuple):
-        print('add_to_node_editor', func.__class__.__name__)
+        # print('add_to_node_editor', func.__class__.__name__)
         self.log.append('Add: ' + func.__class__.__name__)
         self.func = func
         if self.handle is None:
             self.handle = bpy.types.SpaceNodeEditor.draw_handler_add(func, args, 'WINDOW', 'POST_PIXEL')
 
     def remove_from_node_editor(self):
-        print('remove_from_node_editor', self.func.__class__.__name__)
+        # print('remove_from_node_editor', self.func.__class__.__name__)
         if self.handle:
             bpy.types.SpaceNodeEditor.draw_handler_remove(self.handle, 'WINDOW')
             self.handle = None
