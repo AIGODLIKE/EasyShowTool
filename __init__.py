6f35b7988fc6a4e5fff25a6d274b87ee2206d224
1029920178@qq.com <1029910278@qq.com>
2024-07-11 02:32:29 +0800

    change: name

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 bl_info = {
-    "name": "Easy Node Notes",
-    "author": "Atticus",
-    "description": "Adds notes to your node editor",
+    "name": "Easy Show Tool",
+    "author": "Atticus, AIGODLIKE Community",
+    "description": "Add Amazing notes to your node editor",
     "blender": (4, 1, 0),
-    "version": (0, 0, 1),
+    "version": (0, 1, 0),
     "location": "",
     "doc_url": "",
     "warning": "",
