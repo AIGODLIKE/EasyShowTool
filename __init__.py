7365debc0f474a98210361ba1e3ebd14f7027ef1
1029920178@qq.com <1029910278@qq.com>
2024-07-15 15:15:36 +0800

    bump to 0.1.2

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author": "Atticus, AIGODLIKE Community",
     "description": "Add Amazing notes to your node editor",
     "blender": (4, 1, 0),
-    "version": (0, 1, 1),
+    "version": (0, 1, 2),
     "location": "",
     "doc_url": "",
     "warning": "",
