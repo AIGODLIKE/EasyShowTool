cce9b0ada9d24a499a7345ba2eeddd8308bf1469
1029920178@qq.com <1029910278@qq.com>
2024-07-26 10:46:52 +0800

    refactor: area point class

--- model/model_points.py
+++ model/model_points.py
@@ -1,16 +1,30 @@
 from dataclasses import dataclass
-from typing import ClassVar
+from typing import ClassVar, Literal
 
 from mathutils import Vector
+
+PositionType = Literal[
+    'top_left', 'top_right', 'bottom_left', 'bottom_right',
+    'top_center', 'bottom_center', 'left_center', 'right_center'
+]
+
+
+class AreaPoint(Vector):
+    """A point with a position and a type."""
+    position_type: PositionType
+
+    def set_position_type(self, position_type: PositionType) -> 'AreaPoint':
+        self.position_type = position_type
+        return self
 
 
 @dataclass(slots=True)
 class PointsArea:
     """4 points to define an area."""
-    top: int | float = 0 # top y
-    bottom: int | float = 0 # bottom y
-    left: int | float = 0 # left x
-    right: int | float = 0 # right x
+    top: int | float = 0  # top y
+    bottom: int | float = 0  # bottom y
+    left: int | float = 0  # left x
+    right: int | float = 0  # right x
     center: Vector = Vector((0, 0, 0))  # 3d
 
     indices: ClassVar = ((0, 1, 2), (2, 1, 3))  # for gpu batch drawing fan
@@ -27,64 +41,73 @@ class PointsArea:
         self.left = left
         self.right = right
 
+    def get_oppo_area_point(self, point: AreaPoint) -> AreaPoint:
+        match point.position_type:
+            case 'top_left':
+                return self.bottom_right
+            case 'top_right':
+                return self.bottom_left
+            case 'bottom_left':
+                return self.top_right
+            case 'bottom_right':
+                return self.top_left
+            case 'top_center':
+                return self.bottom_center
+            case 'bottom_center':
+                return self.top_center
+            case 'left_center':
+                return self.right_center
+            case 'right_center':
+                return self.left_center
+            case _:
+                raise ValueError(f"Invalid position type: {point.position_type}")
+
     @property
     def size(self) -> Vector:
         return Vector((self.right - self.left, self.top - self.bottom))
 
     @property
-    def top_left(self) -> Vector:
-        return Vector((self.left, self.top))
+    def top_left(self) -> AreaPoint:
+        return AreaPoint((self.left, self.top)).set_position_type('top_left')
 
     @property
-    def top_right(self) -> Vector:
-        return Vector((self.right, self.top))
+    def top_right(self) -> AreaPoint:
+        return AreaPoint((self.right, self.top)).set_position_type('top_right')
 
     @property
-    def bottom_left(self) -> Vector:
-        return Vector((self.left, self.bottom))
+    def bottom_left(self) -> AreaPoint:
+        return AreaPoint((self.left, self.bottom)).set_position_type('bottom_left')
 
     @property
-    def bottom_right(self) -> Vector:
-        return Vector((self.right, self.bottom))
+    def bottom_right(self) -> AreaPoint:
+        return AreaPoint((self.right, self.bottom)).set_position_type('bottom_right')
 
     @property
-    def top_center(self) -> Vector:
-        return (self.top_left + self.top_right) / 2
+    def top_center(self) -> AreaPoint:
+        return ((self.top_left + self.top_right) / 2).set_position_type('top_center')
 
     @property
-    def bottom_center(self) -> Vector:
-        return (self.bottom_left + self.bottom_right) / 2
+    def bottom_center(self) -> AreaPoint:
+        return ((self.bottom_left + self.bottom_right) / 2).set_position_type('bottom_center')
 
     @property
-    def left_center(self) -> Vector:
-        return (self.top_left + self.bottom_left) / 2
+    def left_center(self) -> AreaPoint:
+        return ((self.top_left + self.bottom_left) / 2).set_position_type('left_center')
 
     @property
-    def right_center(self) -> Vector:
-        return (self.top_right + self.bottom_right) / 2
+    def right_center(self) -> AreaPoint:
+        return ((self.top_right + self.bottom_right) / 2).set_position_type('right_center')
 
     @property
-    def corner_points(self) -> tuple[Vector, Vector, Vector, Vector]:
+    def corner_points(self) -> tuple[AreaPoint, AreaPoint, AreaPoint, AreaPoint]:
         """Return the corner points in clockwise"""
         return self.top_left, self.top_right, self.bottom_left, self.bottom_right
 
     @property
-    def corner_points_line_order(self) -> tuple[Vector, Vector, Vector, Vector]:
+    def corner_points_line_order(self) -> tuple[AreaPoint, AreaPoint, AreaPoint, AreaPoint]:
         """Return the corner points in line order, use for draw the bbox line"""
         return self.top_left, self.top_right, self.bottom_right, self.bottom_left
 
     @property
-    def edge_center_points(self) -> tuple[Vector, Vector, Vector, Vector]:
+    def edge_center_points(self) -> tuple[AreaPoint, AreaPoint, AreaPoint, AreaPoint]:
         return self.top_center, self.right_center, self.bottom_center, self.left_center
-
-    @property
-    def corner_points_dict(self) -> dict[str, Vector]:
-        """Return the corner points as a dictionary, use for calc the opposite corner"""
-        return {'top_left': self.top_left, 'top_right': self.top_right,
-                'bottom_left': self.bottom_left, 'bottom_right': self.bottom_right}
-
-    @property
-    def edge_center_points_dict(self) -> dict[str, Vector]:
-        """Return the edge center points as a dictionary, use for calc the opposite edge center"""
-        return {'top_center': self.top_center, 'right_center': self.right_center,
-                'bottom_center': self.bottom_center, 'left_center': self.left_center}
